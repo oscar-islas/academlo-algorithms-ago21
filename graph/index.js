e607853e6555ca454e6305c461a186fd807f58ff
oislasreyes <oislasreyes@gmail.com>
2022-05-24 20:31:11 -0500

    Agregando los metodos bfs y dfs

--- graph/index.js
+++ graph/index.js
@@ -1,17 +1,17 @@
 class Graph {
-  constructor(){
+  constructor() {
     this.adjacencyList = {};
   }
 
-  addVertex(vertex){
-    if(!this.adjacencyList[vertex]){
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
       this.adjacencyList[vertex] = [];
     }
   }
 
-  addEdge(source, destination){
+  addEdge(source, destination) {
     //Dirigido
-    if(!this.adjacencyList[source]){
+    if (!this.adjacencyList[source]) {
       this.addVertex(source);
     }
     this.adjacencyList[source].push(destination);
@@ -26,22 +26,60 @@ class Graph {
     // this.adjacencyList[destination].push(source);
   }
 
-  removeEdge(source, destination){
-    this.adjacencyList[source] = this.adjacencyList[source].filter( vertex => vertex !== destination);
+  removeEdge(source, destination) {
+    this.adjacencyList[source] = this.adjacencyList[source].filter(
+      (vertex) => vertex !== destination
+    );
   }
 
-  removeVertex(vertex){
+  removeVertex(vertex) {
     delete this.adjacencyList[vertex];
   }
 
-  showConnections(source){
+  showConnections(source) {
     const currentNode = this.adjacencyList[source];
     let connections = '';
-    for(let vertex of currentNode){
-      connections += vertex + ','
+    for (let vertex of currentNode) {
+      connections += vertex + ',';
     }
-    console.log(source, '--->', connections)
+    console.log(source, '--->', connections);
   }
+
+  dfsRecursive(vertex, visited){
+    visited[vertex] = true; //Marcar el vertice como visitado
+    console.log(vertex, + '');
+
+    for(let i of this.adjacencyList[vertex].values()){
+      let n = i;
+      if(!visited[n]){
+        this.dfsRecursive(n, visited);
+      }
+    }
+  }
+
+  dfs(vertex){
+    let noVertex = Object.keys(this.adjacencyList).length;
+    const visited = new Array(noVertex).fill(false); //Registro de los nodos que he visitado
+    this.dfsRecursive(vertex, visited);
+  }
+
+  bfs(vertex){
+    let noVertex = Object.keys(this.adjacencyList).length;
+    const visited = new Array(noVertex).fill(false); //Registro de los nodos que he visitado
+    let queue = [vertex];
+    while(queue.length > 0){
+      let visiting = queue.shift(); //Eliminar el primer elemento de la lista
+      let neighbors = this.adjacencyList[visiting];
+      console.log(visiting);
+      for(let i = 0; i < neighbors.length; i++){
+        if(!visited[neighbors[i]]){
+          visited[neighbors[i]] = true; //Marcamos al vertice como visitado
+          queue.push(neighbors[i]); //Agregamos a la cola
+        }
+      }
+    }
+  }
+
 }
 
 const myGraph = new Graph();
@@ -58,4 +96,5 @@ myGraph.addEdge('2', '4');
 myGraph.addEdge('2', '5');
 myGraph.addEdge('3', '4');
 myGraph.addEdge('4', '5');
-myGraph.showConnections('2');
+// myGraph.dfs('0');
+myGraph.bfs('0');
